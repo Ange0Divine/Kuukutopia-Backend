610659e2bd0b228285dff195bcaa4c241aa27807
Ange0Divine <tuyizereangedivine@gmail.com>
2026-06-03 11:51:27 +0200

    fix allowed hosts

--- kuukutopia/settings.py
+++ kuukutopia/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*igq=c*%5k(x+)^#&rq69
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS += ['kuukutopia-backend-1-04j5.onrender.com']
 
 
 # Application definition
